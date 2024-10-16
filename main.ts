1e2960ef8f483a081bcc4ffd91e456a82195ca07
Maria-Varghese <maria.varghese@mths.ca>
2024-10-16 08:24:11 -0700

    this program shows distance

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria
+ * Created on: Sep 2024
+ * This program tells distance using sonar
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+//setup
+basic.showIcon(IconNames.Diamond) 
+
+//finding distance from sonar
+input.onButtonPressed(Button.A, function(){
+    basic.clearScreen()
+    distanceToObject = sonar.ping (
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    basic.showNumber(distanceToObject )
+    basic.showString(' cm')
+    basic.showIcon(IconNames.Happy)
+})
